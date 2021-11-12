7082bcfb527b65f3fde1b1d4ce017112d35dd674
Oskari-Juvakka <oskari.juvakka@gmail.com>
2021-11-12 16:04:05 +0200

    week 2 exercise 2, remove prints

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -9,14 +9,9 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
 
         # toml kirjastolla dictionary
         content_dict = loads(content, _dict=dict)
-
-        print("====================")
-        print(content_dict)
-        print("====================\n")
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
         name = content_dict['tool']['poetry']['name']
